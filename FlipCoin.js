9f4a68d679f58ec153490645a5cce4fd4def8500
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-30 09:45:56 +0530

    Added : IF-ElSE

---
+++ FlipCoin.js
@@ -0,0 +1,9 @@
+// 4.Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly.
+
+var prompt = require('prompt-sync')();
+
+let coin = Math.floor(Math.random()*10) % 2;
+if(coin == 1)
+    console.log("Heads");
+else
+    console.log("Tails");
